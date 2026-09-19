30535876cee6726d0a9e8ae80d56dbe11260880c
tailoreditbd <mamunvai1845@gmail.com>
2026-09-19 12:04:38 +0600

    Update function.js

--- js/function.js
+++ js/function.js
@@ -34,7 +34,7 @@
     $('.treatments-menu > ul > li').has('a[href="hair-treatments/hair-prp"], a[href="hair-treatments/gfc-prp"], a[href="hair-treatments/hair-transplant/hair-prp"], a[href="hair-treatments/hair-transplant/gfc-prp"], a[href="other-treatments/hair-prp"], a[href="other-treatments/gfc-prp"]').remove();
     $('.treatments-menu > ul > li').has('a[href="hair-treatments/hair-transplant"], a[href="hair-transplant/hair-transplant"]').remove();
 
-    var futMenuItem = $('.treatments-menu > ul > li').has('a[href="hair-treatments/hair-transplant/fut-hair-transplant"]');
+    var futMenuItem = $('.treatments-menu > ul > li').has('a[href="hair-treatments/hair-transplant/fut-hair-transplant"], a[href="hair-transplant/method/fut-hair-transplant"], a[href="hair-transplant/fut-hair-transplant"]');
     futMenuItem.find('a').attr('href', 'hair-transplant/method/fut-hair-transplant');
     $('.method-menu > ul').append(futMenuItem);
     $('a[href="hair-treatments/hair-transplant/fut-hair-transplant"], a[href="hair-treatments/method/fut-hair-transplant"], a[href="method/fut-hair-transplant"]').attr('href', 'hair-transplant/method/fut-hair-transplant');
